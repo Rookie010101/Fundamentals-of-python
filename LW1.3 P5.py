5f4dc2e349051b3ffe4bbb2547b7aef0b3a7ebf1
Chirag Modi <chiragmodi2027@gmail.com>
2026-01-06 21:57:04 +0530

    remova all files from master

--- LW1.3 P5.py
+++
@@ -1,5 +0,0 @@
-a = input('Enter a number')
-b = input('Enter second number')
-print(id(a), id(b), a is b)
-a = input('Change first number')
-print(id(a), id(b), a is b)
--- LW1.3_P1.py
+++
@@ -1,5 +0,0 @@
-str1 = input("Enter something")
-print(int(str1), type(str1))
-print(float(str1), type(str1))
-print(bool(str1), type(str1))
-
